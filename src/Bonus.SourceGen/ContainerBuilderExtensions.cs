101848cd7b0e5fe05b79ab1c7c7286ef6d8e97b3
Bonuspunkt <bonuspunkt+github@gmail.com>
2023-03-30 23:28:05 +0200

    "fix" null warning for net48 / net6.0

--- src/Bonus.SourceGen/ContainerBuilderExtensions.cs
+++ src/Bonus.SourceGen/ContainerBuilderExtensions.cs
@@ -11,8 +11,11 @@ namespace Autofac
 #if NET7_0_OR_GREATER
             builder.RegisterModule(T.DelegateModule);
 #else
+#pragma warning disable CS8600,CS8602,CS8604
+            // we have generated this code, so there should be no null issues
             var module = (IModule)typeof(T).GetProperty("DelegateModule").GetValue(null);
             builder.RegisterModule(module);
+#pragma warning restore CS8600,CS8602,CS8604
 #endif
             return builder.RegisterType<T>();
         }
